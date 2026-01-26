49085537bf95f2132deffd6bd9a9059dae03c6f4
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-01-26 15:59:44 -0300

    Adicionado mais uma camada de seguranca utilizando o CORS

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { parseCorsWhitelist } from './common/utils/parse-cors-whitelist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,25 @@ async function bootstrap() {
     }),
   );
 
+  const corsWhiteList = parseCorsWhitelist(process.env.CORS_WHITELIST ?? '');
+
+  app.enableCors({
+    origin: (
+      origin: string | undefined, // Isso é do navegador e para proteger o cliente
+      callback: (...args: any[]) => void,
+    ) => {
+      // Requisição sem origin ou que inclui uma origem conhecida
+      // por corsWhiteList é permitida
+      if (!origin || corsWhiteList.includes(origin)) {
+        return callback(null, true); // Permitido
+      }
+
+      // Requisições com origin mas que não conhecemos
+      // negamos.
+      return callback(new Error('Não habilitado para CORS'), false);
+    },
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -20,6 +40,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.APP_PORT ?? 3001);
 }
 void bootstrap();
